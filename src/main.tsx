7b5694cd3199b99be70464804ecb7afda5894941
Brian Taylor <btaylor@zaius.com>
2018-08-11 14:16:29 -0400

    react window. global world

--- src/main.tsx
+++ src/main.tsx
@@ -2,3 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {World} from './sim';
 
+
+class App extends React.Component<{}, {}> {
+    render() {
+        return <h1>Hello World</h1>;
+    }
+}
+
+let myWindow = window as any;
+myWindow.world = new World();
+
+ReactDOM.render(
+    <App></App>,
+    document.getElementById('app')
+)
